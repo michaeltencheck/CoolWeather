7482af5639d93ae97f8b373aeecc903df5b491a9
michael <neo21anderson@gmail.com>
2015-08-10 13:43:01 +0800

    create a new package
    create a new package under default package

--- app/src/main/java/com/example/test/coolweather/activity/FirstActivity.java
+++ app/src/main/java/com/example/test/coolweather/activity/FirstActivity.java
@@ -1,22 +1,24 @@
-package com.example.test.coolweather;
+package com.example.test.coolweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.test.coolweather.R;
+
+public class FirstActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_first, menu);
         return true;
     }
 
