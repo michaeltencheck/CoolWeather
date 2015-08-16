be5b69dd957f2cd92e2472cf5161aa5b991ba895
michael <neo21anderson@gmail.com>
2015-08-16 14:19:38 +0800

    create province city county class
    create province city county class

---
+++ myweather/src/main/java/com/example/test/myweather/database/County.java
@@ -0,0 +1,34 @@
+package com.example.test.myweather.database;
+
+/**
+ * Created by test on 8/16/2015.
+ */
+public class County {
+    private String county_name;
+    private String county_code;
+    private int city_id;
+
+    public String getCounty_name() {
+        return county_name;
+    }
+
+    public void setCounty_name(String county_name) {
+        this.county_name = county_name;
+    }
+
+    public String getCounty_code() {
+        return county_code;
+    }
+
+    public void setCounty_code(String county_code) {
+        this.county_code = county_code;
+    }
+
+    public int getCity_id() {
+        return city_id;
+    }
+
+    public void setCity_id(int city_id) {
+        this.city_id = city_id;
+    }
+}
---
+++ myweather/src/main/java/com/example/test/myweather/database/Province.java
@@ -0,0 +1,25 @@
+package com.example.test.myweather.database;
+
+/**
+ * Created by test on 8/16/2015.
+ */
+public class Province {
+    private String province_name;
+    private String province_code;
+
+    public String getProvince_name() {
+        return province_name;
+    }
+
+    public void setProvince_name(String province_name) {
+        this.province_name = province_name;
+    }
+
+    public String getProvince_code() {
+        return province_code;
+    }
+
+    public void setProvince_code(String province_code) {
+        this.province_code = province_code;
+    }
+}
