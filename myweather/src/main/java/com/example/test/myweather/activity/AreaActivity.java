331b16633443c6cd57f0944fccb770f3cdddc480
michael <neo21anderson@gmail.com>
2015-08-19 03:22:05 +0800

    create main activity version 1
    create main activity to choose area

--- myweather/src/main/java/com/example/test/myweather/activity/AreaActivity.java
+++ myweather/src/main/java/com/example/test/myweather/activity/AreaActivity.java
@@ -1,18 +1,66 @@
 package com.example.test.myweather.activity;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.test.myweather.R;
+import com.example.test.myweather.database.City;
+import com.example.test.myweather.database.County;
+import com.example.test.myweather.database.MyWeatherDB;
+import com.example.test.myweather.database.Province;
 
-public class AreaActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AreaActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+    public static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+    public static final int LEVEL_COUNTY = 2;
+    
+    private MyWeatherDB myWeatherDB;
+    private TextView textView;
+    private ListView listView;
+    private List<String> dataList = new ArrayList<String>();
+    private ArrayAdapter<String> arrayAdapter;
+    
+    private List<Province> provinceList;
+    private List<City> cityList;
+    private List<County> countyList;
+
+    private Province seletedProvince;
+    private City seletedCity;
+    
+    private int currentLevel;
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area);
+
+        textView = (TextView) findViewById(R.id.area_title_textView);
+        listView = (ListView) findViewById(R.id.choose_area_listView);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataList);
+        listView.setAdapter(arrayAdapter);
+        
+        listView.setOnItemClickListener(this);
+        
+        queryProvince();
+        
+        
+    }
+
+    private void queryProvince() {
     }
 
     @Override
@@ -35,5 +83,22 @@ public class AreaActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if (currentLevel == LEVEL_PROVINCE) {
+            seletedProvince = provinceList.get(position);
+            queryCity();
+        }else if (currentLevel == LEVEL_CITY) {
+            seletedCity = cityList.get(position);
+            queryCounty();
+        }
+    }
+
+    private void queryCounty() {
+    }
+
+    private void queryCity() {
     }
 }
